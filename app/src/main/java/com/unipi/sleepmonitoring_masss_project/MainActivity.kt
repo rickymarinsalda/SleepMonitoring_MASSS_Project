3d0188b1db603ffdc102033370d778fe4deb0691
Froffri <fra.londretti@gmail.com>
2024-05-03 17:50:26 +0200

    Added hamburger_icon_white.xml to toolbar
    Implemented(?) change of fragment form main to menu

--- app/src/main/java/com/unipi/sleepmonitoring_masss_project/MainActivity.kt
+++ app/src/main/java/com/unipi/sleepmonitoring_masss_project/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.unipi.sleepmonitoring_masss_project
 
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -11,25 +15,15 @@ import com.unipi.sleepmonitoring_masss_project.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        val navView: BottomNavigationView = binding.navView
-
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        setContentView(R.layout.activity_main);
     }
+
+    fun openDrawer(view: View) {
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        drawerLayout.openDrawer(GravityCompat.START)
+    }
+
 }
