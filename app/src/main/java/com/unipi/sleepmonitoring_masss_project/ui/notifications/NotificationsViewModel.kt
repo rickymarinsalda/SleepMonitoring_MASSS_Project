b14cd12007ae678754212fe5df0fbf53abb7e4c4
Froffri <fra.londretti@gmail.com>
2024-05-03 16:01:36 +0200

    I bring life as your true god

---
+++ app/src/main/java/com/unipi/sleepmonitoring_masss_project/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.unipi.sleepmonitoring_masss_project.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
