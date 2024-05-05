6b948e1b93eccfe273fb42dac60bd2a1b0c79b0e
dario <dariopag45@gmail.com>
2024-05-05 11:12:32 +0200

    added Ping button (doesn't work bc stupid dep. issues)

---
+++ wear/src/main/java/com/example/wear/MainActivity.kt
@@ -0,0 +1,67 @@
+/* While this template provides a good starting point for using Wear Compose, you can always
+ * take a look at https://github.com/android/wear-os-samples/tree/main/ComposeStarter and
+ * https://github.com/android/wear-os-samples/tree/main/ComposeAdvanced to find the most up to date
+ * changes to the libraries and their usages.
+ */
+
+package com.example.wear
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
+import com.example.wear.presentation.MainApp
+import com.google.android.gms.wearable.PutDataMapRequest
+import com.google.android.gms.wearable.Wearable
+import kotlinx.coroutines.launch
+import java.time.Instant
+import kotlinx.coroutines.*
+class MainActivity : ComponentActivity() {
+    private val dataClient by lazy { Wearable.getDataClient(this) }
+    private val messageClient by lazy { Wearable.getMessageClient(this) }
+    private val capabilityClient by lazy { Wearable.getCapabilityClient(this) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+
+        super.onCreate(savedInstanceState)
+
+        setTheme(android.R.style.Theme_DeviceDefault)
+
+        setContent {
+            MainApp(
+                onPingClicked = ::onPingClicked
+            )
+        }
+    }
+
+    private fun onPingClicked() {
+
+        Log.i(TAG, "Ping clicked!")
+        lifecycleScope.launch {
+            try {
+                val request = PutDataMapRequest.create("/ping-pong").apply {
+                    dataMap.putString("mossa", "ping")
+                    dataMap.putLong("timestamp", Instant.now().toEpochMilli())
+                }
+                    .asPutDataRequest()
+                    .setUrgent()
+
+                val result = dataClient.putDataItem(request).await()
+
+                Log.d(TAG, "DataItem saved: $result")
+            } catch (cancellationException: CancellationException) {
+                throw cancellationException
+            } catch (exception: Exception) {
+                Log.d(TAG, "Saving DataItem failed: $exception")
+            }
+        }
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+}
