7abae73c425c5cedc765a27abf6dd944d3fbac3f
Froffri <fra.londretti@gmail.com>
2024-05-08 12:07:43 +0200

    Changed options and added stats fragment

--- app/src/main/java/com/unipi/sleepmonitoringproject/ui/stats/StatsViewModel.kt
+++ app/src/main/java/com/unipi/sleepmonitoringproject/ui/stats/StatsViewModel.kt
@@ -1,13 +1,13 @@
-package com.unipi.sleepmonitoringproject.ui.gallery
+package com.unipi.sleepmonitoringproject.ui.stats
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class GalleryViewModel : ViewModel() {
+class StatsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is gallery Fragment"
+        value = "This is the stats Fragment"
     }
     val text: LiveData<String> = _text
 }
