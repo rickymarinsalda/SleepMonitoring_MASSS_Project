7ea928baa82a179ad968ba2b410a7ed7acb5b98b
Froffri <fra.londretti@gmail.com>
2024-05-21 15:48:24 +0200

    Implemented the classification of a time series

--- sleeplib/src/main/java/com/unipi/sleepmonitoring_masss_library/classify.kt
+++ sleeplib/src/main/java/com/unipi/sleepmonitoring_masss_library/classify.kt
@@ -1,5 +1,43 @@
 package com.unipi.sleepmonitoring_masss_library
 
 fun classifySeries(classifier: Classifier, series: TimeSeries): IntArray {
-    TODO("dividi in chusdnfksd e classifiojca")
+
+    var results = intArrayOf()
+
+    var i = 0
+    // scan through series
+    while(i < series.size()) {
+
+        lateinit var input : Array<FloatArray>
+        val startTime = series.data[i].timestamp
+
+        var idxToSample : IntArray = intArrayOf()
+
+        var j = i
+        while(j < series.size() && series.data[j].timestamp < startTime + (5 * 60 * 1000L))
+            idxToSample += j++
+
+        if (j >= series.size())
+            return results
+
+        val samples = sampleNValuesFromArray(idxToSample, 5)
+
+        samples.forEach {
+            input += series.data[it].datum
+        }
+        results += classifier.doInference(input)
+
+        i = j
+    }
+
+    return results
+}
+
+fun sampleNValuesFromArray(arr: IntArray, n: Int): List<Int> {
+    if (n > arr.size) {
+        throw IllegalArgumentException("Sample size cannot be greater than array size.")
+    }
+
+    val indices = arr.indices.shuffled().take(n)
+    return indices.map { arr[it] }
 }
