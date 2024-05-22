be91a4e4585414b75500e86147c7f69bb9c3e5e9
Dario Pagani <dario.pagani.146@gmail.com>
2024-05-22 16:22:06 +0200

    change package name episode 2

--- wearapp3/src/main/java/com/unipi/sleepmonitoringproject/MainActivity.kt
+++ wearapp3/src/main/java/com/unipi/sleepmonitoringproject/MainActivity.kt
@@ -1,9 +1,3 @@
-/* While this template provides a good starting point for using Wear Compose, you can always
- * take a look at https://github.com/android/wear-os-samples/tree/main/ComposeStarter and
- * https://github.com/android/wear-os-samples/tree/main/ComposeAdvanced to find the most up to date
- * changes to the libraries and their usages.
- */
-
 package com.unipi.sleepmonitoringproject
 
 import android.annotation.SuppressLint
