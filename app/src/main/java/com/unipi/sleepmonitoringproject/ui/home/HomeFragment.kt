76d9792830eae96daedcfc221dadc4b1460258ae
MartiB28 <martina.burgisi@gmail.com>
2024-05-25 15:35:59 +0200

    Warning fixed

--- app/src/main/java/com/unipi/sleepmonitoringproject/ui/home/HomeFragment.kt
+++ app/src/main/java/com/unipi/sleepmonitoringproject/ui/home/HomeFragment.kt
@@ -2,7 +2,6 @@ package com.unipi.sleepmonitoringproject.ui.home
 
 import android.graphics.Color
 import android.graphics.Typeface
-import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -10,7 +9,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.TextView
-import androidx.annotation.RequiresApi
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
 import androidx.core.content.res.ResourcesCompat
